473e74638c0cbfe60bf717f73e075f55c1224abe
ItsTheOneAJ <aaolivares@ucsd.edu>
2022-04-22 11:03:23 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-
+import java.util.List;
 import org.junit.*;
 
 public class MarkdownParseTest {
